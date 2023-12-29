85a06f4fe4b3e714369b1254d7ccbe72fa0eb68a
Xcilik <149920327+Xcilik@users.noreply.github.com>
2023-12-29 15:02:21 +0700

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -73,7 +73,7 @@ def fsub_button(client, message):
     if FORCE_SUB_CHANNEL and not FORCE_SUB_GROUP:
         buttons = [
             [
-                InlineKeyboardButton(text="Join Channel 1", url="https://t.me/Bisniscuaninvestasisuksesid"),             
+                InlineKeyboardButton(text="Join Channel 1", url="https://t.me/+_lZyGwyjWuhkZTBl"),             
             ],
             [
                 InlineKeyboardButton(text="Join Channel 2", url=client.invitelink),             
