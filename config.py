cb1070fd0eae16088b5ac9630f11119b7fa691c4
Xcilik <149920327+Xcilik@users.noreply.github.com>
2024-01-09 14:13:30 +0700

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ from logging.handlers import RotatingFileHandler
 load_dotenv("config.env")
 
 # Bot token dari @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6835281316:AAGuzMri0MWaTstgxXA-VZTlKGs3xtjGN0g")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6835281316:AAFQtZtyQCywMSLdVkfe4s_vzjSzIsiQVxA")
 
 # API ID Anda dari my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "21445722"))
@@ -39,7 +39,7 @@ UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 DB_URI = os.environ.get("DATABASE_URL", "postgres://tylabkta:2vXf_y3UOrqglGckq-XlZEnbDiZy83_R@lucky.db.elephantsql.com/tylabkta")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002083690848"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002039064200"))
 FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
