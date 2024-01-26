b211b4799957cbadc49553d0e549b8f7535cfb7c
Xcilik <149920327+Xcilik@users.noreply.github.com>
2024-01-26 12:39:59 +0700

    Update config.py

--- config.py
+++ config.py
@@ -40,6 +40,7 @@ DB_URI = os.environ.get("DATABASE_URL", "postgres://tylabkta:2vXf_y3UOrqglGckq-X
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002039064200"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "0"))
 FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
